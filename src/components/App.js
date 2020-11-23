70ac57b447e87126fbc7e4a01f31b9a7d5d8b4bc
Devgupta0 <emailidabc3>
2020-11-23 21:58:24 +0530

    Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -5,56 +5,40 @@ import {useLocation} from 'react-router-dom';
 import '../styles/App.css';
 
 
-export function LocationDisplay(){
+function LocationDisplay(){
     let location=useLocation();
     return <div data-testid="location-display">{location.pathname}</div>
 }
-export function Home(){
-    return <>
-        <LocationDisplay/>
-        <div style={{backgroundColor: 'orange', width: "100%", height: "500px"}}>You are home.</div>
-        </>
+function Home(){
+    return <div style={{backgroundColor: 'orange', width: "100%", height: "500px"}}>You are home.</div>
+        
 }
-export function About(){
-    
-    return <>
-        <LocationDisplay/>
-        <div style={{backgroundColor: 'green', width: "100%", height: "500px"}}>You are on the about page.</div>
-        </>
+function About(){
+    return <div style={{backgroundColor: 'green', width: "100%", height: "500px"}}>You are on the about page.</div>
 }
-export function NotFound(){
-    
-    return <div>No match</div>
-}
+
 export default class App extends Component {
-    constructor(props){
-        super();
-    }
     render() {
         return(
            
             <div id="main">
-             <BrowserRouter>
-                <Link to="/">HOME/</Link>
-                <Link to="/about">ABOUT</Link>
-                <Switch>
-                    <Route exact path='/'>
-                    
-                        <Home />
-                    </Route>
-                    <Route path='/about'>
-                    
-                        <About />
-                    </Route>
-                    <Route>
-                        <NotFound/>
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-
+                <BrowserRouter>
+                    <LocationDisplay/>
+                    <Link to="/">HOME/</Link>
+                    <Link to="/about">ABOUT</Link>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Home />
+                        </Route>
+                        <Route exact path='/about'>
+                            <About />
+                        </Route>
+                        <Route path='*'>
+                           No match
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
             </div>
-            
-        )
-        
+        );
     }
 }
