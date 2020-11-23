94e4f96fa4e4d078ccefef62b529fd56b909d22c
Devgupta0 <emailidabc3>
2020-11-23 18:47:50 +0530

    nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,46 @@
 import React, {Component, useState} from "react";
-import {BrowserRouter,Link,Route,Switch,withRouter} from 'react-router-dom';
-import Home from './Home.js';
-import About from './About.js';
+import {Link,Route,Switch,useLocation} from 'react-router-dom';
 import '../styles/App.css';
 
+
+export  function LocationDisplay(){
+    let location  = useLocation();
+    return <div data-testid="location-display">{location.pathname}</div>
+}
+export  function Home(){
+    return
+    <>
+        <LocationDisplay/>
+        <div style={{background:'orange',width:"100%",height:"500px"}}>
+            You are on home.
+        </div>
+    </>
+}
+export  function About(){
+
+    return
+    <>
+        <LocationDisplay/>
+        <div style={{background:'green',width:"100%",height:"500px"}}>
+            You are on the about page.
+        </div>
+    </>
+}
+export  function NotFound(){
+    return <div> No match</div>
+   
+}
+
 class App extends Component {
+    constructor(props){
+        super();
+    }
     render() {
 
         return(
-            <BrowserRouter>
                 <div id="main">
-                    <nav>
-                        
-                                <Link to="/">HOME/</Link>
-                            
-                                <Link to="/about">ABOUT/</Link>
-                            
-                                <Link>Contact-us</Link>
-                            
-                    </nav>
-                    
-                    
-                    
+                    <Link to="/">HOME</Link> 
+                    <Link to="/about">ABOUT/</Link>
                     <Switch>
                         <Route exact path="/">
                             <Home/>
@@ -29,12 +48,17 @@ class App extends Component {
                         <Route path="/about">
                             <About />
                         </Route>
+                        <Route>
+                            <NotFound/>
+                        </Route>
                     </Switch>
                 </div>
-            </BrowserRouter>
-        );
+            )
     }
 }
+
+
+
 
 
 export default App;
