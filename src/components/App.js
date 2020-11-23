6ada0420c686f7c9a0d0046eea1f527738c6d21f
Devgupta0 <emailidabc3>
2020-11-23 14:14:53 +0530

    nav

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ class App extends Component {
                             
                                 <Link to="/about">ABOUT/</Link>
                             
-                                <Link>CONTACT-US</Link>
+                                <Link>Contact-us</Link>
                             
                     </nav>
                     
