4c99e37275e8f89998c200cb2ebc934c8da8d34a
Devgupta0 <emailidabc3>
2020-11-23 21:38:24 +0530

    navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React, {Component, useState} from "react";
-import {Link, Route, Switch } from 'react-router-dom';
+import {Link, Route, Switch,BrowserRouter } from 'react-router-dom';
 import {useLocation} from 'react-router-dom';
-// import Home from './Home.js';
-// import About from './About.js';
+
 import '../styles/App.css';
-// import NotFound from "./NotFound.js";
+
 
 export function LocationDisplay(){
     let location=useLocation();
@@ -33,25 +32,28 @@ export default class App extends Component {
     }
     render() {
         return(
-            // <BrowserRouter>
+           
             <div id="main">
-            <Link to="/">HOME/</Link>
-            <Link to="/about">ABOUT</Link>
-            <Switch>
-                <Route exact path='/'>
-                   
-                    <Home />
-                </Route>
-                <Route path='/about'>
-                
-                    <About />
-                </Route>
-                <Route>
-                    <NotFound/>
-                </Route>
-            </Switch>
+             <BrowserRouter>
+                <Link to="/">HOME/</Link>
+                <Link to="/about">ABOUT</Link>
+                <Switch>
+                    <Route exact path='/'>
+                    
+                        <Home />
+                    </Route>
+                    <Route path='/about'>
+                    
+                        <About />
+                    </Route>
+                    <Route>
+                        <NotFound/>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+
             </div>
-            //  </BrowserRouter>
+            
         )
         
     }
